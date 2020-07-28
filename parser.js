bd1bb8c0e88dc7876503eb65bdaf9872c52bde92
Azamat Tikenov <azamatkz90@gmail.com>
2020-07-29 00:37:37 +0600

    Initialize project using Create React App

---
+++ parser.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const XLSX = require("xlsx");
+const schema = require("./data/schema");
+
+const readFileContent = (file) => {
+  const table = XLSX.readFile(folder + file);
+  const sheet = table.Sheets[table.SheetNames[0]];
+  const range = XLSX.utils.decode_range(sheet["!ref"]);
+  range.s.r = 3;
+  for (var r = range.s.r; r <= range.e.r; ++r) {
+    let row = {};
+    for (var c = range.s.c; c <= range.e.c; ++c) {
+      let cell_ref = XLSX.utils.encode_cell({ c, r });
+      row[schema[c].name] = sheet[cell_ref] ? sheet[cell_ref].v : null;
+    }
+    row.row = r;
+    row.fn = file;
+    result.count++;
+    result.data.push(row);
+  }
+};
+
+const folder = "./data/";
+const result = { count: 0, data: [] };
+
+fs.readdirSync(folder).forEach((file) => {
+  if (/\.xlsx$/.test(file)) readFileContent(file);
+});
+
+fs.writeFile(folder + "data.json", JSON.stringify(result), (err) => {
+  if (err) throw err;
+  console.log("*** data.json is saved.");
+});
